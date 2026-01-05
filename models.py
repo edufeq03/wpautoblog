de4269a7e9e36b335ff341ce46c125e88205d948
Eduardo Capella <edufeq03@gmail.com>
2026-01-05 04:21:44 -0300

    feat: ajuste no models para postgres

--- models.py
+++ models.py
@@ -9,7 +9,8 @@ login_manager = LoginManager()
 
 @login_manager.user_loader
 def load_user(user_id):
-    return User.query.get(int(user_id))
+    # O .get() do session é mais seguro e compatível com Postgres
+    return db.session.get(User, int(user_id))
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
